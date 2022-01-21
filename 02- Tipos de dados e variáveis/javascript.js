86836d62984e456004aa93d05b55757decc69532
Pedro Henrique Santos Araujo <pedrohenriquesan001@gmail.com>
2022-01-21 19:31:22 -0300

    Tipos de dados e variáveis

---
+++ 02- Tipos de dados e variáveis/javascript.js
@@ -0,0 +1,50 @@
+/*O código escrito abaixo foi feito e testado na ferramenta nodeJS, a qual permite a execução de códigos em JavaScript fora de um navegador, cada "Bloco de código" foi referente ao assunto comentado acima do mesmo, foi versionado para fins de organização.*/
+
+/*Cada linha desse código deve ser jogada uma a uma no nodeJS para serem testadas*/ 
+
+//Qualquer coisa que for digitada com aspas simples, aspas duplas ou crases serão devolvidas pelo node.
+"Oi"
+'Oi'    
+`Oi`
+
+//Operações aritméticas são feitas de forma automática.
+7+3
+7-3  
+7*3
+7/3
+
+//Criação, valoração, alteração e vizualização de variável.
+var nome = "Pedro Henrique Santos"      //criação e valoração
+nome                                    //vizualização
+'nome'                                  //teste com aspas (provando que da erro)
+nome = "Pedrin Malvadão"                //alteração
+nome                                    
+
+//Valores decimais usam o ponto e não a vírgula, cuidado. 
+var decimal = 2.56
+decimal                 
+decimal = 2,56
+decimal
+
+//A soma dos dados de uma variável ocorre da mesma forma dos números. 
+var n1 = 7  
+var n2 = 3      
+n1+n2
+
+//Letras maiúsculas e minúsculas tem diferença. 
+var a = 10
+A               
+a
+
+//Null remete ao vazio.
+var b = null    
+b
+
+//Typeof é um operador da linguagem que permite saber o tipo de dado de variáveis e dados.
+var nome = "Pedro Henrique"
+nome
+typeof nome                     
+nome = 12
+nome
+typeof nome
+
