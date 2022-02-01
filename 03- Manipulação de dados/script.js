5142dea1f4c481da10a22c0f5ef3968178f43872
Pedro Henrique Santos Araujo <pedrohenriquesan001@gmail.com>
2022-02-01 17:38:48 -0300

    Manipulação de dados 1

---
+++ 03- Manipulação de dados/script.js
@@ -0,0 +1,8 @@
+var nome = window.prompt("POR FAVOR, DIGITE SEU NOME:")     /*armazenando o nome do usuário*/ 
+window.alert('Seja bem-vindo ' + nome + '!')                /*exibindo o nome do usuário armazenado na variável 'nome'*/
+
+var n1 = window.prompt("POR FAVOR, DIGITE UM NÚMERO:")      /*armazenando o primeiro número*/
+var n2 = window.prompt("POR FAVOR, DIGITE OUTRO NÚMERO")    /*armazenando o segundo número*/
+var soma = n1 + n2                                          /*operação aritmética entre as variáveis com resultado em uma nova variável*/
+window.alert("Sua soma é = " + soma)                        /*exibindo o resultado da manipulação dos dados armazenados*/ 
+
