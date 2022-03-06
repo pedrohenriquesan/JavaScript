b847bf50835808b8506bc1d86798a0cd49372370
Pedro Henrique Santos Araujo <pedrohenriquesan001@gmail.com>
2022-03-06 10:40:14 -0300

    Atualização Eventos DOM

--- 06- Eventos DOM/script.js
+++ 06- Eventos DOM/script.js
@@ -1,6 +1,24 @@
 /*EVENTOS E FUNÇÕES EM JAVASCRIPT*/
+
+var div = window.document.getElementById('area')
+div.addEventListener('click', clicar)
+div.addEventListener('mouseenter', entrar)
+div.addEventListener('mouseout', sair)
+
 function clicar() {
     var div = window.document.getElementById('area')
-    div.innerText = "Click!"
+    div.innerText = "Clicou"
+    div.style.backgroundColor = "red"
+}
+
+function entrar() {
+    var div = window.document.getElementById('area')
+    div.innerText = "Entrou"
+}
+
+function sair() {
+    var div = window.document.getElementById('area')
+    div.innerText = "Saiu"
+    div.style.backgroundColor = "rgb(59, 137, 111)"
 }
 
