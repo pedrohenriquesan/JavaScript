1b2c7300b6ac58f76583a2d33e0cb43775eb6999
Pedro Henrique Santos Araujo <pedrohenriquesan001@gmail.com>
2023-03-16 19:42:01 -0300

    while e do while

---
+++ 10- Repetições em JS/script.js
@@ -0,0 +1,12 @@
+var i = 0
+
+while (i < 3) {                 /*ESTRUTURA DE REPETIÇÃO WHILE*/
+    console.log(`Volta ${i}`)
+    i++
+}
+
+do {                            /*ESTRUTURA DE REPETIÇÃO DO WHILE*/
+    console.log(`Volta ${i}`)
+    i++
+} while (i < 3)
+
