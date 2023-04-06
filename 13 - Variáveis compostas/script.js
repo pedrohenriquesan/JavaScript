a4328e9d7025d9fea808094b46eb5615833cc330
Pedro Henrique Santos Araujo <pedrohenriquesan001@gmail.com>
2023-04-06 20:29:04 -0300

    Vetores

---
+++ 13 - Variáveis compostas/script.js
@@ -0,0 +1,18 @@
+var num = [5, 8, 2, 9, 3]   /*CRIAÇÃO VETOR*/
+
+console.log(`Nosso vetor inicial é: ${num}`)
+
+num[5] = 6      /*ADIÇÃO DE VALOR, SABENDO O ÍNDICE*/
+
+console.log(`Nosso vetor com 10 é: ${num}`)
+
+num.push(7)     /*ADIÇÃO DE VALOR NA ÚLTIMA POSIÇÃO*/
+
+console.log(`Nosso vetor com 7 é: ${num}`)
+
+console.log(`Nosso vetor possui ${num.length} posições`)    /*TAMANHO DO VETOR*/
+
+num.sort()      /*ORDENA O VETOR EM ORDEM CRESCENTE*/
+
+console.log(`Nosso vetor ordenado é: ${num}`)
+
